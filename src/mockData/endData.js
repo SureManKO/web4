2e5cbaae9347d4a436522f3912d62b943782c111
SureManKo <mshurmanko@gmail.com>
2024-03-21 22:40:04 +0500

    done

---
+++ src/mockData/endData.js
@@ -0,0 +1,103 @@
+const upperContainerData = 
+{
+  header: "Хотите Шагнуть в Будущее Раньше Других?",
+  cta: "Запросить ранний доступ",
+  href: "#"
+};
+
+const leftLowerContainerData = 
+{
+  img: {
+    src: "assets/img/logo.svg",
+    alt: "GPT-4",
+  },
+  adress: {
+    street: "ул. Профессора Поздеева, 13.к.Г",
+    city: "Пермь, Пермский край, 614013",
+  },
+  reservedRights: "Все права защищены",
+};
+
+const subHrefData = 
+[
+  {
+      title: "Овероны",
+      href: "#"
+  },
+  {
+      title: "Соц. сети",
+      href: "#"
+  },
+  {
+      title: "Счетчики",
+      href: "#"
+  },
+  {
+      title: "Контакты",
+      href: "#"
+  }
+];
+
+const subCompanyData = 
+[
+  {
+      title: "Условия использования",
+      href: "#"
+  },
+  {
+      title: "Перс. данные",
+      href: "#"
+  },
+  {
+      title: "Конаткты",
+      href: "#"
+  }
+];
+
+const subContactsData = 
+[
+  {
+      title: "ул. Профессора Поздеева, 13, к.Г, Пермь",
+      href: ""
+  },
+  {
+      title: "+7 (342) 2-198-520",
+      href: ""
+  },
+  {
+      title: "info@pstu.ru",
+      href: ""
+  }
+];
+
+const rightLowerContainerData = 
+[
+  {
+      header: "Ссылки",
+      sub: subHrefData
+  },
+  {
+      header: "Компания",
+      sub: subCompanyData
+  },
+  {
+      header: "Контакты",
+      sub: subContactsData
+  }
+];
+
+
+const licenseData = 
+{
+  title: "© 2023 GPT-4 Все права защищены."
+};
+
+const endData = 
+{
+  upperContainerData,
+  leftLowerContainerData,
+  rightLowerContainerData,
+  licenseData
+};
+
+export default endData;
